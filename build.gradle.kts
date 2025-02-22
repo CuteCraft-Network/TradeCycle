12f0585aa4798bd0db57797058fb83f9d5b792f6
S42yt <songoku42@outlook.de>
2025-02-22 15:50:40 +0300

    fix plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
-    kotlin("jvm") version "2.0.2.1"
+    id("org.jetbrains.kotlin.jvm") version "1.9.22"
 }
 
 group = "s42.site"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    mavenCentral()
     maven {
         name = "papermc"
         url = uri("https://repo.papermc.io/repository/maven-public/")
@@ -13,12 +14,20 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
 }
 
 tasks {
-    processResources { filesMatching("plugin.yml") { expand("version" to project.version) } }
-    jar { archiveFileName.set("${rootProject.name}-${project.name}-${project.version}.jar") }
+    processResources {
+        filesMatching("plugin.yml") { expand("version" to project.version) }
+    }
+    jar {
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        archiveFileName.set("${rootProject.name}-${project.name}-${project.version}.jar")
+        from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    }
 }
 kotlin {
     jvmToolchain(21)
--- src/main/kotlin/s42/site/tradeCycler/TradeCycler.kt
+++ src/main/kotlin/s42/site/tradeCycler/TradeCycler.kt
@@ -1,53 +1,46 @@
 package s42.site.tradeCycler
 
+import org.bukkit.Bukkit
 import org.bukkit.entity.Villager
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.inventory.InventoryType
-import org.bukkit.event.player.PlayerSwapHandItemsEvent
-import org.bukkit.inventory.MerchantInventory
+import org.bukkit.event.player.PlayerInteractEntityEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class TradeCycler : JavaPlugin(), Listener {
 
-    private var cycleKey: String = "R"
-
     override fun onEnable() {
         saveDefaultConfig()
-        loadConfiguration()
-        server.pluginManager.registerEvents(this, this)
-    }
-
-    private fun loadConfiguration() {
-        val configuredKey = config.getString("button.type")
-        if (configuredKey.isNullOrEmpty()) {
-            logger.warning("No key configured in config.yml! Using 'R' as default.")
-            cycleKey = "R"
-        } else {
-            cycleKey = configuredKey.uppercase()
-        }
+        Bukkit.getPluginManager().registerEvents(this, this)
     }
 
     @EventHandler
-    fun onKeyPress(event: PlayerSwapHandItemsEvent) {
+    fun onInteract(event: PlayerInteractEntityEvent) {
         val player = event.player
-        val inventory = player.openInventory.topInventory
+        val entity = event.rightClicked
 
-        if (inventory.type != InventoryType.MERCHANT) return
+        if (entity !is Villager) return
+        if (!player.isSneaking) return
+
+        if (hasBeenTraded(entity)) {
+            player.sendActionBar("§cCannot cycle trades - villager has been traded with!")
+            return
+        }
+
         event.isCancelled = true
 
-        val merchantInv = inventory as MerchantInventory
-        val trader = merchantInv.merchant
-        if (trader !is Villager) return
+        val profession = entity.profession
+        entity.profession = Villager.Profession.NONE
+        entity.profession = profession
 
-        when {
-            !trader.isTrading -> {
-                trader.recipes.shuffle()
-                player.sendMessage("§aTrades cycled!")
-                player.closeInventory()
-                player.openMerchant(trader, true)
-            }
-            else -> player.sendMessage("§cCannot cycle trades - villager has been traded with!")
-        }
+        player.sendActionBar("§aTrades cycled!")
+
+        Bukkit.getScheduler().runTaskLater(this, Runnable {
+            player.openMerchant(entity, true)
+        }, 2L)
+    }
+
+    private fun hasBeenTraded(villager: Villager): Boolean {
+        return villager.recipes.any { it.uses > 0 }
     }
 }
